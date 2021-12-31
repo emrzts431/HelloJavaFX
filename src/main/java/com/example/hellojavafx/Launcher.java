fe6251abaab08ce05de5c1ab538bbd742de2f8d0
Stephane <tchonlafistephane9@gmail.com>
2021-12-31 23:29:50 +0100

    Kommentar Hinzugefügt

--- src/main/java/com/example/hellojavafx/Launcher.java
+++ src/main/java/com/example/hellojavafx/Launcher.java
@@ -4,7 +4,6 @@ public class Launcher {
     public static void main(String[] args){
         HelloApplication.main(args);
         //Frohes neues Jahres
-        //Frohes neues Jahres
-        //Frohes neues Jahres
+
     }
 }
